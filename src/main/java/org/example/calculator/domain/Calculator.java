98b257183ee0745043e9a7a43dac004112a01c2e
dev-taeyeong <dev.taeyeong@gmail.com>
2023-02-11 14:46:21 +0900

    was practice

---
+++ src/main/java/org/example/calculator/domain/Calculator.java
@@ -0,0 +1,21 @@
+package org.example.calculator.domain;
+
+import org.example.calculator.tobe.AdditionOperator;
+import org.example.calculator.tobe.ArithmeticOperator;
+import org.example.calculator.tobe.DivisionOperator;
+import org.example.calculator.tobe.SubtractionOperator;
+
+import java.util.List;
+
+public class Calculator {
+
+    private static final List<ArithmeticOperator> arithmeticOperators =
+        List.of(new AdditionOperator(), new DivisionOperator(), new SubtractionOperator());
+    public static int calculate(PositiveNumber operand1, String operator, PositiveNumber operand2) {
+        return arithmeticOperators.stream()
+            .filter(arithmeticOperators -> arithmeticOperators.supports(operator))
+            .map(arithmeticOperators -> arithmeticOperators.calculate(operand1, operand2))
+            .findFirst()
+            .orElseThrow(() -> new IllegalArgumentException("올바른 사칙연산이 아닙니다."));
+    }
+}
