dea7e625dbeccb57d3b3ee71ea0c10455c5451f2
dev-taeyeong <dev.taeyeong@gmail.com>
2023-02-11 15:28:58 +0900

    Thread pool 적용

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         // new CustomWebApplicationServer(8080).start(); // v1
-        new CustomWebApplicationServerV2(8080).start(); // v2
+        // new CustomWebApplicationServerV2(8080).start(); // v2
+        new CustomWebApplicationServerV3(8080).start();
     }
 }
